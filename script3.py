1a705fe1e1a1699f47902533a24ecb2e15aa8b34
Alan D <31675641+alanmicah@users.noreply.github.com>
2023-03-06 14:28:06 +0000

    minimax making a move

--- script3.py
+++ script3.py
@@ -1,10 +1,23 @@
+from hashlib import new
 from tic_tac_toe import *
 from copy import deepcopy
+
+new_game = [
+	["1", "2", "3"],
+	["4", "5", "6"],
+	["7", "8", "9"]
+]
 
 x_winning = [
 	["X", "2", "O"],
 	["4", "O", "6"],
 	["7", "8", "X"]
+]
+
+o_winning = [
+	["X", "X", "O"],
+	["4", "X", "6"],
+	["7", "O", "O"]
 ]
 
 def game_is_over(board):
@@ -24,10 +37,13 @@ def minimax(input_board, is_maximizing):
         return evaluate_board(input_board)
     if is_maximizing:
         best_value = -float('Inf')
+        symbol = "X"
     else:
         best_value = float('Inf')
+        symbol = "O"
     for i in available_moves(input_board):
-        print(i)
-    return best_value
+        new_board = deepcopy(input_board)
+        select_space(new_board, i, symbol)
+    return new_board
 
 print(minimax(x_winning, True))
